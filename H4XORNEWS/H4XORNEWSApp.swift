208fcbb3c6cb9aa6ccf578433371388618f11042
Sreekanth <sreekanthps28@gmail.com>
2023-03-01 12:39:43 +0000

    Initial Commit

---
+++ H4XORNEWS/H4XORNEWSApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORNEWSApp.swift
+//  H4XORNEWS
+//
+//  Created by SREEKANTH PS on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct H4XORNEWSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
